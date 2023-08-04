9d6cad64f2419a007618971095e0d69cc8a55504
Guido097 <112497761+Guido097@users.noreply.github.com>
2023-08-04 13:40:12 -0300

    updating main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, File, UploadFile, Path
+from fastapi.responses import JSONResponse
+import json
+import pandas as pd
+from dateutil.parser import parse
+import ast
+import csv
+import codecs
+
+app = FastAPI()
