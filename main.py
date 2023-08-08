ca6c6e3f776c5c87c5c80ff3bfb1481b69eff847
Guido097 <112497761+Guido097@users.noreply.github.com>
2023-08-08 13:14:56 -0300

    'Finishing details in main.py'

--- main.py
+++ main.py
@@ -56,59 +56,85 @@ unique_release_years_str = [str(year) for year in unique_release_years]
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"Hi! This is a proyect for the car": "Hello World"}
 
 @app.get('/genre/')
 def genero(year : str):
-    df_filtrado = df[df["release_year"] == int(year)]
-    df_filtrado["genres"] = df_filtrado["genres"].str.replace(r'\[|\]', '').str.replace("'", "").str.split(", ")
-    lista_generos = [genero for sublist in df_filtrado["genres"] for genero in sublist]
-    generos_lanzados = pd.Series(lista_generos).value_counts()
-    top_5_generos = generos_lanzados.head(5)
-    return top_5_generos.index.tolist()
+    if int(year) in unique_release_years:
+        df_filtrado = df[df["release_year"] == int(year)]
+        df_filtrado["genres"] = df_filtrado["genres"].str.replace(r'\[|\]', '').str.replace("'", "").str.split(", ")
+        lista_generos = [genero for sublist in df_filtrado["genres"] for genero in sublist]
+        generos_lanzados = pd.Series(lista_generos).value_counts()
+        top_5_generos = generos_lanzados.head(5)
+        return top_5_generos.index.tolist()
+    else:
+        return {"Error!: The introduced year is not in database"}
 
 @app.get('/games/')
 def juegos(year : str):
-    df_filtrado = df[df["release_year"] == int(year)]
-    df_filtrado["app_name"] = df_filtrado["app_name"].str.replace(r'\[|\]', '').str.replace("'", "").str.split(", ")
-    juegos_lanzados = df_filtrado["app_name"].tolist()
-    return juegos_lanzados
+    if int(year) in unique_release_years:
+        df_filtrado = df[df["release_year"] == int(year)]
+        df_filtrado = df_filtrado.drop_duplicates(subset=['app_name'])
+        df_filtrado["app_name"] = df_filtrado["app_name"].str.replace(r'\[|\]', '').str.replace("'", "").str.split(", ")
+        juegos_lanzados = df_filtrado["app_name"].tolist()
+        return juegos_lanzados
+    else:
+        return {"Error!: The introduced year is not in database"}
 
 @app.get('/specs/')
 def specs(year: str):
-    df_filtrado = df[df["release_year"] == int(year)]
-    lista_specs = [spec for sublist in df_filtrado["specs"] for spec in sublist]
-    freq_specs = pd.Series(lista_specs).value_counts()
-    top_5_specs = freq_specs.head(5)
-    resultado_dict = top_5_specs.to_dict()
-    return resultado_dict
+    if int(year) in unique_release_years:
+        df_filtrado = df[df["release_year"] == int(year)]
+        df_filtrado = df_filtrado.drop_duplicates(subset=['app_name'])
+        lista_specs = [spec for sublist in df_filtrado["specs"] for spec in sublist]
+        freq_specs = pd.Series(lista_specs).value_counts()
+        top_5_specs = freq_specs.head(5)
+        resultado_dict = top_5_specs.to_dict()
+        return resultado_dict
+    else:
+        return {"Error!: The introduced year is not in database"}
 
 @app.get('/earlyaccess/')
 def earlyacces(year: str):
-    '''df["release_date"] = pd.to_datetime(df["release_date"])'''
-    df_filtrado = df[df["release_year"] == int(year)]
-    juegos_early_access = df_filtrado["early_access"].sum()
-    return print('En el año', year, 'fueron lanzados', juegos_early_access, 'con early access.')
+    if int(year) in unique_release_years:
+        df_filtrado = df[df["release_year"] == int(year)]
+        df_filtrado = df_filtrado.drop_duplicates(subset=['app_name'])
+        juegos_early_access = df_filtrado["early_access"].sum()
+        if juegos_early_access == 0:
+            return "That year were not released games with early access"
+        else:
+            return f"That year, {juegos_early_access} were released with early access."
+    else:
+        return {"Error!: The introduced year is not in database"}
+
 
 @app.get('/sentiment/')
 def sentiment(year: str):
     if int(year) in unique_release_years:
         df_filtrado = df[df["release_year"] == int(year)]
+        df_filtrado = df_filtrado.drop_duplicates(subset=['app_name'])
         sentiment_counts = df_filtrado["sentiment"].value_counts()
         filtered_sentiments = sentiment_counts.index[~sentiment_counts.index.str.contains("user reviews")]
         filtered_counts = sentiment_counts.loc[filtered_sentiments]
         resultado_dict = filtered_counts.to_dict()
         return resultado_dict
-    if not (year) in unique_release_years or unique_release_years_str:
-        return year, 'No es un valor que se encuentre en la base de datos.'
+    else:
+        return {"Error!: The introduced year is not in database"}
 
 @app.get('/metascore/')
 def metascore(year: str):
-    df_filtrado = df[df["release_year"] == int(year)]
-    df_filtrado["metascore"] = pd.to_numeric(df_filtrado["metascore"])
-    top_5_juegos = df_filtrado.nlargest(5, "metascore")
-    resultados = top_5_juegos.set_index("app_name")["metascore"].to_dict()
-    return resultados
+    if int(year) in unique_release_years:
+        df_filtrado = df[df["release_year"] == int(year)]
+        df_filtrado = df_filtrado.drop_duplicates(subset=['app_name'])
+        df_filtrado["metascore"] = pd.to_numeric(df_filtrado["metascore"])
+        top_5_juegos = df_filtrado.nlargest(5, "metascore")
+        resultados = top_5_juegos.set_index("app_name")["metascore"].to_dict()
+        return resultados
+    else:
+        return {"Error!: The introduced year is not in database"}
+
+
+
 
 
 #Limpio la columna Metascore y normalizo
@@ -131,45 +157,19 @@ lower_limit = Q1 - 1.5 * IQR
 upper_limit = Q3 + 1.5 * IQR
 df_outliers = df[(df['price'] >= lower_limit) & (df['price'] <= upper_limit)]
 
-#Creo el modelo de ML
+#Creo el modelo de ML para que el RMSE quede cargado en la variable "rmse"
 X = df_outliers[['encoded_genres', 'early_access', 'release_year', 'metascore']]
 y = df_outliers['price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 # Crear y ajustar el modelo
 model = lgb.LGBMRegressor(random_state=42)
 model.fit(X_train, y_train)
-
 # Predecir el precio en el conjunto de prueba
 y_pred = model.predict(X_test)
 force_row_wise = True
-# Calcular el coeficiente de determinación (R^2)
-r2 = r2_score(y_test, y_pred)*100
-
 # Calcular la raíz del error cuadrado medio (RMSE)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
-@app.get('/predict')
-async def predict_price(genre , early_access : bool , metascore : int, year : int):
-    #encoded_genres = label_encoder_genres.transform([genre])[0]
-    genre_encoded = label_encoder_genres.transform([genre])[0]
-    early_access = early_access.lower() == "true"
-    input_df = pd.DataFrame({
-        "early_access": [early_access],
-        "genre": [genre_encoded],
-        "metascore": [metascore],
-        "release_year": [year]
-    })
-
-       
-    predicted_price = model.predict(input_df)[0]
-    return {"predicted_price": predicted_price , 'RMSE:':rmse}
-
-
-
-#Carga de modelo
-with open('lightgbm_model.pkl', 'wb') as f:
-    pickle.dump(model, f)
 
 with open('lightgbm_model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
